8453c6ac030fa3d058d9d49e7fb737aa3cceca26
Muath Alshathri <abogbl4@gmail.com>
2023-05-15 01:19:31 +0300

    Update and rename next.config.mjs to next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  /**
+   * Tell Next.js where the `public` folder is.
+   */
+  assetPrefix: isProd ? "/chat-app-client/" : "",
+  /**
+   * Disable server-based image optimization.
+   *
+   * @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
+   */
+  images: {
+    unoptimized: true,
+  },
+};
