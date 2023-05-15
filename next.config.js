5a6328a8daaad458c604b4b1991009b88ec6198e
Muath Alshathri <abogbl4@gmail.com>
2023-05-15 10:01:11 +0300

    updated gh-pages

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ module.exports = {
   basePath: process.env.NODE_ENV === 'production' ? '/chat-app-client' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/chat-app-client/' : '',
   output: 'export',
+  publicRuntimeConfig: {
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/your-repo-nchat-app-clientame/' : '',
+  },
 };
