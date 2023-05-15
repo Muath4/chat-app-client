c3a1b471c8b9c5946c4b8f47b619d61e62cfd295
Muath Alshathri <abogbl4@gmail.com>
2023-05-15 11:19:45 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-// next.config.js
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  // Use the prefix for GitHub Pages
   basePath: isProd ? '/chat-app-client' : '',
   assetPrefix: isProd ? '/chat-app-client/' : '',
   publicRuntimeConfig: {
@@ -12,5 +10,4 @@ module.exports = {
     loader: 'imgix',
     path: isProd ? 'https://muath4.github.io/chat-app-client/' : '/',
   },
-  target: 'server',
 };
