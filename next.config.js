bff740686e766b0fe99fbfb3d19dc3cb2f379542
Muath Alshathri <abogbl4@gmail.com>
2023-05-15 09:46:48 +0300

    updated gh-pages

--- next.config.js
+++ next.config.js
@@ -2,4 +2,5 @@ module.exports = {
   // Use the prefix for GitHub Pages
   basePath: process.env.NODE_ENV === 'production' ? '/chat-app-client' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/chat-app-client/' : '',
+  output: 'export',
 };
